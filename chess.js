3c31229c957775bc7828b7b378949bf37397f7a1
Marcin Rał <marcin@ral.info.pl>
2018-10-05 19:01:37 +0200

    step one

--- chess.js
+++ chess.js
@@ -1,1 +1,24 @@
 console.log('Welcome to Chess!');
+
+var chessboard = '';
+
+for(var h=1;h<=8;h++){
+    for(var v=1;v<=8;v++){
+        if(h%2===0){
+            if(v%2===0){
+                chessboard += '#';
+            } else {
+                chessboard += ' ';
+            }
+        } else{
+            if(v%2===0){
+                chessboard += ' ';
+            } else {
+                chessboard += '#';
+            }
+        }
+    }
+    chessboard += '\n';
+}
+
+console.log(chessboard);
